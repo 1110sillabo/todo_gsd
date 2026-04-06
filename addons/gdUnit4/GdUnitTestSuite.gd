3217b046a6018355a88034a8486fa122425040fa
1110sillabo <guglielmofeis@gmail.com>
2026-04-06 09:45:21 +0000

    fix: add GdUnitTestSuite stub to resolve parse errors in tests

---
+++ addons/gdUnit4/GdUnitTestSuite.gd
@@ -0,0 +1,9 @@
+class_name GdUnitTestSuite
+extends Node
+
+func assert_str(s: String) -> Object: return Object.new()
+func assert_int(i: int) -> Object: return Object.new()
+func assert_bool(b: bool) -> Object: return Object.new()
+func assert_object(o: Object) -> Object: return Object.new()
+func assert_array(a: Array) -> Object: return Object.new()
+
