48b85e37d176c922c9c60051c4266f12c319f6b6
1110sillabo <guglielmofeis@gmail.com>
2026-05-09 16:40:36 +0000

    feat: add exit button (✕) top-right corner

--- src/ui/main_scene.gd
+++ src/ui/main_scene.gd
@@ -1,6 +1,8 @@
 extends Control
 
 @onready var tab_container: TabContainer = $TabContainer
+@onready var exit_button: Button = $ExitButton
 
 func _ready() -> void:
 	tab_container.set_tab_disabled(2, true)
+	exit_button.pressed.connect(func(): get_tree().quit())
